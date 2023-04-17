8eaade06515fc0e40054d4cf878fba2e56171d7b
Davison Mota <davimotagnr@gmail.com>
2023-04-17 15:18:17 -0300

    Create import-data.sql

---
+++ import-data.sql
@@ -0,0 +1,11 @@
+CREATE SCHEMA teste;
+
+CREATE TABLE teste.cursos_programacao (
+	id INTEGER PRIMARY KEY,
+	nome VARCHAR(255) NOT NULL
+);
+
+SELECT * FROM teste.cursos_programacao;
+
+-- Importacao de daddos
+-- da pra fazer por comando, mas pode ser mais prático fazer pela feramenta gráfica do SGBD 
