51917648728f78c0bb1de00320cf40a04269fa63
Davison Mota <davimotagnr@gmail.com>
2023-05-03 14:28:04 -0300

    Update transaction-rollback-commit.sql

--- transaction-rollback-commit.sql
+++ transaction-rollback-commit.sql
@@ -9,3 +9,78 @@ DELETE FROM aluno_curso WHERE aluno_id = 1 AND curso_id = 4;
 COMMIT; -- confirmando a transacao
 
 SELECT * FROM aluno_curso;
+
+-- TRANSACTION não são suportadas dentro da função, te se usar quando chama a função
+CREATE OR REPLACE FUNCTION criar_intrutor() RETURNS instrutor AS $$
+	BEGIN
+	END;
+$$ LANGUAGE plpgsql;
+
+
+START TRANSACTION;
+select * criar_intrutor();
+COMMIT; --ou ROLLBACK
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
